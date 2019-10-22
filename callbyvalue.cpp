3d1d84b9d598860547141da001f4e8917ce5c6a9
Ishita <54714611+ishitamahe@users.noreply.github.com>
2019-10-22 23:22:20 +0530

    Add files via upload

---
+++ callbyvalue.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void swap(int x,int y);
+int main()
+{
+	int x,y;
+	printf("Enter two numbers: ");
+	scanf("%d%d",&x,&y);
+	printf("\nBefore swapping \n");
+	printf("Value of x= %d",x);
+	printf("\nValue of y= %d",y);
+	swap(x,y);
+	printf("\n\nAfter swapping \n");
+	printf("Value of x= %d",x);
+	printf("\nValue of y= %d",y);
+}
+void swap(int x,int y)
+{
+	int t;
+	t=x;
+	x=y;
+	y=t;
+	printf("\n\nInside swap function \n");
+	printf("Value of x= %d",x);
+	printf("\nValue of y= %d",y);
+}
